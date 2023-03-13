07033ddb5a9d2864b55adce2c6d451cfa2ce6e77
= <=>
2023-03-13 15:05:51 -0300

    Check if the user is logged

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 // using tcc.Models;
@@ -23,6 +24,7 @@ public class HomeController : Controller
         return View();
     }
 
+    [Authorize]
     public async Task<IActionResult> Info()
     {
         var user = await _userManager.GetUserAsync(User);
